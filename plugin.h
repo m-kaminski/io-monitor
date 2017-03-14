95f75988f8e8878b673dbbe78f46529eedb495ac
Paul Dardeau <pauldardeau@gmail.com>
2017-03-14 12:53:08 -0400

    add plugin header and sample plugin source

---
+++ plugin.h
@@ -0,0 +1,18 @@
+#ifndef PLUGIN_H
+#define PLUGIN_H
+
+#define PLUGIN_OPEN_SUCCESS  0
+#define PLUGIN_OPEN_FAIL 1
+
+#define PLUGIN_ACCEPT_DATA  1
+#define PLUGIN_REFUSE_DATA  0
+
+/*
+int open_plugin(const char* plugin_config);
+void close_plugin();
+int ok_to_accept_data();
+int process_data(struct monitor_record_t* data);
+*/
+
+#endif
+
